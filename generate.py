720b274f19638ca3ae588c87edb410103deb07aa
Mitanshu Sukhwani <mitanshu.sukhwani@gmail.com>
2025-06-29 22:40:09 +0530

    added code to generate model answers, tested it in main

---
+++ generate.py
@@ -0,0 +1,100 @@
+from string import Template
+
+"""
+Generate answers with local and API models.
+"""
+from email import message
+
+from transformers import pipeline
+import torch
+from openai import OpenAI
+
+# TODO: ADD Logger
+
+
+def run_inference_local(
+    model_name: str,
+    query: str,
+    template: str = "",
+    max_new_tokens: int = 200,
+    temperature: float = 0.1,
+) -> str:
+    """
+    Function to run text generation inference locally using a specified model.
+
+    Parameters:
+    model_name (str): Name of the model to use
+    query (str): Input text for the model
+    template (str): System prompt
+    max_new_tokens (int): Maximum length of the generated text (default: 100)
+    temperature (float): Creativity of LLM from 0.0-1.0. Higher is less accurate.
+
+    Returns:
+    dict: The model response
+    """
+
+    # Select GPU if available
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Load the model
+    generator = pipeline("text-generation", model=model_name, device=device)
+
+    messages = [
+        {"role": "system", "content": template},
+        {"role": "user", "content": query},
+    ]
+
+    # Get response
+    response = generator(
+        messages,
+        return_full_text=False,
+        max_new_tokens=max_new_tokens,
+        temperature=temperature,
+    )
+
+    return response[0]["generated_text"]
+
+
+def run_inference_api(
+    url: str,
+    api_key: str,
+    model_name: str,
+    query: str,
+    template: str = "",
+    max_new_tokens: int = 200,
+    temperature: float = 0.0,
+) -> str:
+    """
+    Function to query OpenAI compatible API with a prompt and return the response.
+
+    Parameters:
+    url (str): The OpenAI API endpoint URL
+    api_key (str): Your OpenAI API key
+    model_name (str): Name of the model to use
+    template (str): System prompt
+    query (str): Input text for the model
+    max_new_tokens (int):  Maximum length of the generated text (default: 100)
+    temperature (float): Creativity of LLM from 0.0-1.0. Higher is less accurate.
+
+    Returns:
+    dict: The API response converted from JSON
+    """
+
+    client = OpenAI(
+        base_url=url,
+        api_key=api_key,
+    )
+
+    messages = [
+        {"role": "system", "content": template},
+        {"role": "user", "content": query},
+    ]
+
+    completion = client.chat.completions.create(
+        model=model_name,
+        messages=messages,
+        max_completion_tokens=max_new_tokens,
+        temperature=temperature,
+    )
+
+    return completion.choices[0].message.content
--- main.py
+++ main.py
@@ -1,6 +1,48 @@
-def main():
-    print("Hello from readability!")
+from dotenv import dotenv_values
+from generate import run_inference_api, run_inference_local
 
+config = dotenv_values(".env")
 
-if __name__ == "__main__":
-    main()
+OPENROUTER_API_KEY = config["OPENROUTER_API_KEY"]
+
+output = run_inference_api(
+    "https://openrouter.ai/api/v1",
+    OPENROUTER_API_KEY,
+    "meta-llama/llama-3.2-1b-instruct:free",
+    "2+2=?",
+    # "Only tell me the result and have nothing else in the output",
+)
+
+print(output)
+
+print("*" * 80)
+
+output = run_inference_api(
+    "https://openrouter.ai/api/v1",
+    OPENROUTER_API_KEY,
+    "meta-llama/llama-3.2-1b-instruct:free",
+    "2+2=?",
+    "Only tell me the result and have nothing else in the output",
+)
+
+print(output)
+
+# print("*" * 80)
+
+# output = run_inference_local(
+#     "meta-llama/llama-3.2-1b-instruct",
+#     "2+2=?",
+#     # "Only tell me the result and have nothing else in the output",
+# )
+
+# print(output)
+
+# print("*" * 80)
+
+# output = run_inference_local(
+#     "meta-llama/llama-3.2-1b-instruct",
+#     "2+2=?",
+#     "Only tell me the result and have nothing else in the output",
+# )
+
+# print(output)
