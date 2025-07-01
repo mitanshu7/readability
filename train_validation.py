11b75fc15bc41c4e09f12be5f6c9b4e94e696017
Mitanshu Sukhwani <mitanshu.sukhwani@gmail.com>
2025-07-02 00:16:28 +0530

    Add svm model for embedding + readability input

--- train_validation.py
+++ train_validation.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import (
     train_test_split,  # Split data into train and test sets
 )
 from sklearn.svm import SVC  # Classification model
+import numpy as np # For array manipulation
 
 df = pd.read_parquet("datasets/OneStopEnglish/OneStopEnglish.parquet")
 
@@ -83,3 +84,41 @@ print(f"Paragraph Readability SVM Accuracy: {paragraph_readability_svm_accuracy}
 # Save the svm models using pickle
 with open("models/OneStopEnglish/paragraph_readability_svm_model.pkl", "wb") as f:
     dump(paragraph_readability_svm, f)
+
+################################################################################
+
+# Read data from parquet files
+df_embed = pd.read_parquet("datasets/OneStopEnglish/OneStopEnglish.parquet")
+df_read = pd.read_parquet("datasets/OneStopEnglish/OneStopEnglish_readability.parquet")
+
+# Add readability scores to the embedding dataframe
+df_embed["text_readability_scores"] = df_read["text_readability_scores"]
+
+# Drop paragraph related columns
+df = df_embed.drop(["paragraph", "paragraph_embedding"], axis=1)
+
+# Extend the embedding vector with the readability scores
+print("Extending embedding vector...")
+df["text_vector_readability"] = df.apply(
+    lambda row: np.concatenate((row["text_embedding"], row["text_readability_scores"])),
+    axis=1
+)
+
+# Split the data into training and testing sets
+train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
+
+# Initialize and train the SVM model
+text_vector_readability_svm = SVC()
+text_vector_readability_svm.fit(
+    train_df["text_vector_readability"].tolist(), train_df["level"]
+)
+
+# Find accuracy
+text_vector_readability_svm_accuracy = text_vector_readability_svm.score(
+    test_df["text_vector_readability"].tolist(), test_df["level"]
+)
+print(f"Text Vector Readability SVM Accuracy: {text_vector_readability_svm_accuracy}")
+
+# Save the svm models using pickle
+with open("models/OneStopEnglish/text_vector_readability_svm_model.pkl", "wb") as f:
+    dump(text_vector_readability_svm, f)
