4f69f3cac14e9a4d3a3b3d6f2a48b92e2ec1e39c
Mitanshu Sukhwani <mitanshu.sukhwani@gmail.com>
2025-07-01 16:13:22 +0530

    add onestopenglish dataset csv

---
+++ train_validation.py
@@ -0,0 +1,68 @@
+"""
+Train Models to classify reading level of a text
+"""
+
+from glob import glob  # Gather files
+
+import pandas as pd  # For data maipulation
+
+# Gather files from the dataset
+OneStopEnglish = glob("datasets/OneStopEnglish/**/*.txt")
+print(f"Gathered {len(OneStopEnglish)} files")
+
+
+# Function to extract Level of text
+def extract_level(row: pd.DataFrame) -> str:
+    # Get foldername for the level
+    foldername = row["filename"].split("/")[-2]
+
+    # Get level name from folder, e.g. Adv, Ele, or Int.
+    level = foldername.split("-")[0]
+
+    return level
+
+
+# Function to extract text from files
+def extract_text(row: pd.DataFrame) -> str:
+    # Read contents from file name
+    filename = row["filename"]
+    with open(filename, "r") as f:
+        text = f.read()
+
+    # Remove the first line, since it only contains the level of the text
+    text = text.split("\n", 1)[1]
+
+    return text
+
+
+# Function to extract paragraphs from text
+def extract_paragraph(text: str) -> list[str]:
+    # Split text into paragraphs
+    paragraphs = text.split("\n")
+
+    # Remove empty paragraphs
+    paragraphs = [p for p in paragraphs if p.strip()]
+
+    return paragraphs
+
+
+# Generate a pandas dataframe for classification
+df = pd.DataFrame({"filename": OneStopEnglish})
+
+# Extract level
+df["level"] = df.apply(extract_level, axis=1)
+
+# Extract the text from the file
+df["text"] = df.apply(extract_text, axis=1)
+
+df.to_csv("datasets/OneStopEnglish/all.csv", index=False)
+
+# Extract paragraph from text
+df["paragraph"] = df["text"].apply(extract_paragraph)
+
+# Create multiple rows from one
+df = df.explode("paragraph")
+
+df[["filename", "level", "paragraph"]].to_csv(
+    "datasets/OneStopEnglish/all_paragraphs.csv", index=False
+)
