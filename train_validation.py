c106458d978e3f16774829b70303a62260f33fe4
Mitanshu Sukhwani <mitanshu.sukhwani@gmail.com>
2025-07-01 22:22:50 +0530

    add svm by only using a list of readability scores as input

--- train_validation.py
+++ train_validation.py
@@ -15,7 +15,9 @@ df = pd.read_parquet("datasets/OneStopEnglish/OneStopEnglish.parquet")
 # Split data into train and test sets
 train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
 
-# Train SVM
+################################################################################
+
+# Train SVM for full text
 text_svm = SVC()
 text_svm.fit(train_df["text_embedding"].tolist(), train_df["level"])
 
@@ -23,7 +25,13 @@ text_svm.fit(train_df["text_embedding"].tolist(), train_df["level"])
 text_svm_accuracy = text_svm.score(test_df["text_embedding"].tolist(), test_df["level"])
 print(f"Text SVM Accuracy: {text_svm_accuracy}")
 
-# Train SVM for paragraphs
+# Save the text svm model using pickle
+with open("models/OneStopEnglish/text_svm_model.pkl", "wb") as f:
+    dump(text_svm, f)
+
+################################################################################
+
+# Train SVM for paragraphs extracted from text
 paragraph_svm = SVC()
 paragraph_svm.fit(train_df["paragraph_embedding"].tolist(), train_df["level"])
 
@@ -34,8 +42,44 @@ paragraph_svm_accuracy = paragraph_svm.score(
 print(f"Paragraph SVM Accuracy: {paragraph_svm_accuracy}")
 
 # Save the svm models using pickle
-with open("models/OneStopEnglist/text_svm_model.pkl", "wb") as f:
-    dump(text_svm, f)
-
-with open("models/OneStopEnglist/paragraph_svm_model.pkl", "wb") as f:
+with open("models/OneStopEnglish/paragraph_svm_model.pkl", "wb") as f:
     dump(paragraph_svm, f)
+
+################################################################################
+
+df = pd.read_parquet("datasets/OneStopEnglish/OneStopEnglish_readability.parquet")
+
+# Split data into train and test sets
+train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
+
+# Train svm on readability scores of full text
+readability_svm = SVC()
+readability_svm.fit(train_df["text_readability_scores"].tolist(), train_df["level"])
+
+# Find accuracy
+readability_svm_accuracy = readability_svm.score(
+    test_df["text_readability_scores"].tolist(), test_df["level"]
+)
+print(f"Readability SVM Accuracy: {readability_svm_accuracy}")
+
+# Save the svm models using pickle
+with open("models/OneStopEnglish/readability_svm_model.pkl", "wb") as f:
+    dump(readability_svm, f)
+
+################################################################################
+
+# Train svm on readability scores of paragraphs
+paragraph_readability_svm = SVC()
+paragraph_readability_svm.fit(
+    train_df["paragraph_readability_scores"].tolist(), train_df["level"]
+)
+
+# Find accuracy
+paragraph_readability_svm_accuracy = paragraph_readability_svm.score(
+    test_df["paragraph_readability_scores"].tolist(), test_df["level"]
+)
+print(f"Paragraph Readability SVM Accuracy: {paragraph_readability_svm_accuracy}")
+
+# Save the svm models using pickle
+with open("models/OneStopEnglish/paragraph_readability_svm_model.pkl", "wb") as f:
+    dump(paragraph_readability_svm, f)
